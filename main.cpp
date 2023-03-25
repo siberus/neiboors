29794b9bbf877e239576c828f43b1bfc350415ad
siberus <mr.siberus@yandex.ru>
2023-03-25 16:23:06 +0700

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int temp;
+    vector<int> arr;
+    bool flag = true;
+    while (cin >> temp)
+    {
+        arr.push_back(temp);
+        char c = cin.get();
+        if (c == '\n')
+            break;
+    }
+
+        for (int i = 0; i < arr.size() - 1; i++) {
+            if (arr.at(i) >= arr.at(i + 1)) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+        {
+            cout << "YES";
+        }
+        else
+            cout << "NO";
+
+    return 0;
+}
